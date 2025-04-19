464b735daa90407bccc006f8a4949c0c1458edab
Francesco Girlanda <f.girlanda22@gmail.com>
2025-04-19 12:28:49 +0200

    aggiunto file Registrazione.java

---
+++ TheKnife/src/TheKnife.java
@@ -0,0 +1,9 @@
+import fileJava.Login;
+import javafx.stage.Stage;
+
+public class TheKnife {
+    public static void main(String[] args) throws Exception {
+        Login login = new Login();
+        login.start(new Stage());
+    }
+}
