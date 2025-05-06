6aab23cee9bf4162771110cb86054150897feb32
Francesco Girlanda <f.girlanda22@gmail.com>
2025-05-06 13:35:53 +0200

    fix import

--- TheKnife-Maven/the_knife/src/main/java/com/gruppo10/classi/utenteWriter.java
+++ TheKnife-Maven/the_knife/src/main/java/com/gruppo10/classi/utenteWriter.java
@@ -3,7 +3,8 @@ package com.gruppo10.classi;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import com.opencsv.bean.*;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
 
 import com.opencsv.CSVWriter;
 
@@ -11,7 +12,7 @@ public class utenteWriter {
     
     public void scriviUtente(Utente utente) throws IOException {
         Writer writer = new FileWriter("/fileCSV/utenti.csv", true);
-        StateFullBeanToCsv<Utente> stateFullBeanToCsv = new StateFullBeanToCsv<Utente>(writer).build();
+        StatefulBeanToCsv<Utente> statefulBeanToCsv = new StatefulBeanToCsvBuilder<Utente>(writer).build();
         // try (CSVWriter csvWriter = new CSVWriter(writer)) {
         //     String[] record = {utente.getNome(), utente.getCognome(), utente.getUsername(), utente.getPassword(), utente.getDataDiNascita().toString(), utente.getIndirizzo(), utente.getRuolo().name()};
         //     csvWriter.writeNext(record);
