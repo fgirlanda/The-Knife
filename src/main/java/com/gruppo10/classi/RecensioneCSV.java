c3296288dd5cfaa43bc36b292ae524f8c9d86c18
fgirlanda <f.girlanda22@gmail.com>
2025-08-21 00:31:35 +0200

    fix intestazione

--- src/main/java/com/gruppo10/classi/RecensioneCSV.java
+++ src/main/java/com/gruppo10/classi/RecensioneCSV.java
@@ -1,7 +1,7 @@
 /*
- * Francesco Girlanda 760616 VA
- * Gabriele Gallon 761125 VA
- * Mattia Lambertoni 762595 VA
+ Francesco Girlanda 760616 VA
+ Gabriele Gallon 761125 VA
+ Mattia Lambertoni 762595 VA
  */
 package com.gruppo10.classi;
 
